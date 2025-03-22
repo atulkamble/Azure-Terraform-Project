b96eb84f5601113496641e3657ff477a01270c90
Atul Kamble <atul_kamble@hotmail.com>
2025-03-22 10:05:39 +0530

    project-code

--- main.tf
+++ main.tf
@@ -22,6 +22,14 @@ resource "azurerm_subnet" "web" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_public_ip" "web_pip" {
+  name                = "web-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
 resource "azurerm_network_interface" "web_nic" {
   name                = "web-nic"
   location            = azurerm_resource_group.rg.location
@@ -29,17 +37,10 @@ resource "azurerm_network_interface" "web_nic" {
 
   ip_configuration {
     name                          = "web-ip-config"
-    subnet_id                    = azurerm_subnet.web.id
+    subnet_id                     = azurerm_subnet.web.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.web_pip.id
   }
-}
-
-resource "azurerm_public_ip" "web_pip" {
-  name                = "web-pip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_security_group" "web_nsg" {
@@ -58,6 +59,18 @@ resource "azurerm_network_security_group" "web_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface_security_group_association" "web_nic_nsg" {
@@ -66,13 +79,18 @@ resource "azurerm_network_interface_security_group_association" "web_nic_nsg" {
 }
 
 resource "azurerm_linux_virtual_machine" "web_vm" {
-  name                  = "web-vm"
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = azurerm_resource_group.rg.location
-  size                  = "Standard_B1s"
-  admin_username        = var.admin_username
-  network_interface_ids = [azurerm_network_interface.web_nic.id]
-  admin_password        = var.admin_password
+  name                            = "web-vm"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_B1s"
+  admin_username                  = var.admin_username
+  network_interface_ids           = [azurerm_network_interface.web_nic.id]
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("~/.ssh/id_rsa.pub") # Replace with actual path if different
+  }
 
   os_disk {
     caching              = "ReadWrite"
@@ -88,15 +106,16 @@ resource "azurerm_linux_virtual_machine" "web_vm" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-get update",
-      "sudo apt-get install -y nginx"
+      "sudo apt-get update -y",
+      "sudo apt-get install -y nginx",
+      "echo '<h1>Provisioned by Terraform (Atul)</h1>' | sudo tee /var/www/html/index.html"
     ]
 
     connection {
-      type     = "SSH"
-      user     = var.admin_username
-      password = var.admin_password
-      host     = azurerm_public_ip.web_pip.ip_address
+      type        = "ssh"
+      user        = var.admin_username
+      private_key = file("~/.ssh/id_rsa") # Replace with actual path if different
+      host        = azurerm_public_ip.web_pip.ip_address
     }
   }
 }
